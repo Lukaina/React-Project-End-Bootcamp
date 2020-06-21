5b3157a97f43047122f80d49b0e7bf5ab71edfdf
Vero <lukainaf@gmail.com>
2020-06-21 17:32:07 -0500

    Change and add new photos

--- src/Pages/Babies/index.js
+++ src/Pages/Babies/index.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import DndWrapper from '../../components/DND-Wrapper';
-import Image from './nino.jpeg';
+import Image1 from './bebe1.jpg';
+import Image2 from './bebe2.jpg';
 
 const items = [
     {
-        name: 'superman',
+        name: 'Minnie Mouse',
         size: '10-12',
         price: '$20',
         state: 'disponible',
-        image: Image
+        image: Image1
+    },
+    {
+        name: 'Perrito',
+        size: '10-12',
+        price: '$20',
+        state: 'disponible',
+        image: Image2
     },
     {
         name: 'superman',
         size: '10-12',
         price: '$20',
         state: 'disponible',
-        image: Image
+        image: Image1
     },
     {
         name: 'superman',
         size: '10-12',
         price: '$20',
         state: 'disponible',
-        image: Image
-    }    
+        image: Image1
+    },
+    {
+        name: 'superman',
+        size: '10-12',
+        price: '$20',
+        state: 'disponible',
+        image: Image1
+    },   
 ]
 
 export default function Babies(){
