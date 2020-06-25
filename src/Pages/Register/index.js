6034cb0d539e2fab5ead411200fc073a44076c72
Vero <lukainaf@gmail.com>
2020-06-25 15:25:45 -0500

    Elaboration button continue and route to form

---
+++ src/Pages/Register/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../../components/Form';
+
+class Register extends React.Component {
+    render () {
+        return (
+            <div className="Register">
+                <Form />
+            </div>
+        );
+    }
+}
+
+export default Register;
+
--- src/components/DND-Wrapper/index.js
+++ src/components/DND-Wrapper/index.js
@@ -1,56 +1,57 @@
-import React, { PureComponent } from "react";
-
-import Panel from "../Panel";
-
-import "./styles.css";
-
-export default class DndWrapper extends PureComponent {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            leftItems: [...props.items],
-            rightItems: []
-          };
-    }
-
-  onDropCard = (stateName, items) => info => {
-    const { addedIndex, removedIndex, payload } = info;
-    console.log({info,stateName})
-    if (addedIndex === null && removedIndex === null) {
-      return;
-    }
-
-    const result = [...items];
-
-    if (removedIndex !== null) {
-        // eslint-disable-next-line no-unused-expressions
-      result.splice(removedIndex, 1)[0];
-    }
-
-    if (addedIndex !== null) {
-      result.splice(addedIndex, 0, payload);
-    }
-
-    this.setState({ [stateName]: result });
-  };
-
-  render() {
-    const { leftItems, rightItems } = this.state;
-
-    return (
-      <div className="dnd-wrapper">
-        <Panel
-          getChildPayload={index => leftItems[index]}
-          items={leftItems}
-          onDrop={this.onDropCard("leftItems", leftItems)}
-        />
-        <Panel
-          getChildPayload={index => rightItems[index]}
-          items={rightItems}
-          onDrop={this.onDropCard("rightItems", rightItems)}
-        />
-      </div>
-    );
-  }
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
+ 
+import Panel from "../Panel"; 
+ 
+import "./styles.css"; 
+ 
+export default function DndWrapper({ items }) { 
+  const [leftItems, setLeftItems] = useState([...items]); 
+  const [rightItems, setRightItems] = useState([]);
+  const history = useHistory();
+ 
+  const onDropCard = (stateName, items) => (info) => { 
+    const { addedIndex, removedIndex, payload } = info; 
+ 
+    if (addedIndex === null && removedIndex === null) { 
+      return; 
+    } 
+ 
+    const result = [...items]; 
+ 
+    if (removedIndex !== null) { 
+        // eslint-disable-next-line no-unused-expressions 
+      result.splice(removedIndex, 1)[0]; 
+    } 
+ 
+    if (addedIndex !== null) { 
+      result.splice(addedIndex, 0, payload); 
+    } 
+ 
+    if (stateName === 'leftItems') { 
+      setLeftItems(result); 
+    } else { 
+      setRightItems(result); 
+    } 
+  } 
+ 
+  return ( 
+      <div className="dnd-wrapper"> 
+        <div className="dnd-wrapper__content"> 
+          <Panel 
+            getChildPayload={index => leftItems[index]} 
+            items={leftItems} 
+            onDrop={onDropCard("leftItems", leftItems)} 
+          /> 
+          <Panel 
+            getChildPayload={index => rightItems[index]} 
+            items={rightItems} 
+            onDrop={onDropCard("rightItems", rightItems)} 
+          /> 
+        </div> 
+        <div className="dnd-wrapper__btn"> 
+          <button onClick={() => history.push('/registro') }>Continuar</button> 
+        </div> 
+      </div> 
+    ); 
 }
