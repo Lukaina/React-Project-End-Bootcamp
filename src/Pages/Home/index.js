b20b19e4b1308dba456ec752afa143b919df1078
Melissa <melissa_gallo82151@elpoli.edu.co>
2020-07-08 17:20:53 -0500

    agregar home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import CardImages from '../../components/Cards/CardsImages';
 import { CustomMap } from '../../components/custom-mapa/Mapa';
-
+import CustomText from '../../components/custom-textoHome/TexHome'; 
 class App extends React.Component {
     render () {
         return (
               <div className="page page-home">
               <section className="page__content">
                 <CardImages />
+                <CustomText/>
                 <CustomMap /> 
               </section>
           </div>
